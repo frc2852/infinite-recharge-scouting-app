4cf3f1e5ea245290110cc042f304445dedcbca83
Matt Felice <matthew.marino.felice@gmail.com>
2020-01-11 07:08:44 +0000

    Added html loader

---
+++ helpers/html-loader.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (folder, plugins) => {
+    const files = fs.readdirSync(folder);
+
+    if (folder[folder.length - 1] !== '/') {
+        folder += '/';
+    }
+
+    return files
+        .map(file => {
+            console.log({ template: folder + file, filename: file });
+            return new HtmlWebpackPlugin({ template: folder + file, filename: file });
+        })
+        .concat(plugins);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlLoader = require('./helpers/html-loader');
 
 const srcPath = path.resolve('src') + '/';
 const outputFolder = path.resolve('dist') + '/';
@@ -9,26 +10,26 @@ module.exports = {
     entry: srcPath + 'js/app.js',
     output: {
         path: outputFolder,
-        filename: 'app.js'
+        filename: 'app.js',
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
             },
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                loader: 'babel-loader' 
-            }
-        ]
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
     },
-    plugins: [
+    plugins: htmlLoader(srcPath + 'pages', [
         new MiniCssExtractPlugin({
             filename: 'main.css',
-            chunkFilename: 'main.css'
+            chunkFilename: 'main.css',
         }),
-        new HtmlWebpackPlugin({ template: srcPath + 'index.html', filename: 'index.html' })
-    ]
-}
+        new HtmlWebpackPlugin({ template: srcPath + 'index.html', filename: 'index.html' }),
+    ]),
+};

bff809f0cdf9cb455f59909cb39e8d3421e07945
lucs100 <lucs100d@gmail.com>
2020-01-14 19:00:38 -0500

    Fixed ids, added reference file

---
+++ src/reference.js
@@ -0,0 +1,21 @@
+// to define a var:
+
+const Balls = 3;
+
+// do NOT use Balls ++
+// vars can only be edited using:
+
+let Balls = 3;
+balls ++;
+
+// to create a function:
+
+function addBall(number){
+      return number + 1;
+}
+
+// if statements:
+
+if (balls<3){
+      balls = addBall(balls);
+}

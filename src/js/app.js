0bf8e0b0120ee0f4ee05895479259eb438c878c7
lucs100 <lucs100d@gmail.com>
2020-01-14 19:35:53 -0500

    Started with jQuery code, created dummy alerts

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 import '../main.css';
+import $ from 'jquery';          //uses $ as a variable for jquery
+
+$(document).ready(function(){    // forces jquery to wait until the site is ready
+      $('#rotation-successful').click(function(){      // # means go to; look at the rotation successful key
+            alert('hello button');                     // and when it is clicked, run the function (run alert)
+      })
+
+      $('.btn-high-goal').click(function(){   // . means in this class, meaning this fires for all in this class
+            alert('hello class');
+      })
+});
+

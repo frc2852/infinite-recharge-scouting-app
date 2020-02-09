936a53aea2c18b529681b847488220a9dc20a4a9
Matt Felice <matthew.marino.felice@gmail.com>
2020-02-09 20:03:21 +0000

    Added download schedule page

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -3,24 +3,25 @@ import validResponse from './functions/valid-response';
 const OFFLINE_PAGES = 'OFFLINE_PAGES';
 
 self.addEventListener('fetch', event => {
-    event.respondWith(async function () {
-        if (navigator.onLine) {
-            try {
-                const response = await fetch(event.request);
-                if (validResponse(response)) {
-
-                    if (event.request.method === "GET") {
-                        const responseToCache = response.clone();
-                        await caches.open(OFFLINE_PAGES).then(cache => cache.put(event.request, responseToCache))
-                    }
-
-                    return response;
-                }
-            } catch (e) {
-                console.log(e);
+  event.respondWith(
+    (async function() {
+      if (navigator.onLine) {
+        try {
+          const response = await fetch(event.request);
+          if (validResponse(response)) {
+            if (event.request.method === 'GET' && event.request.url.indexOf('firestore') === -1) {
+              const responseToCache = response.clone();
+              await caches.open(OFFLINE_PAGES).then(cache => cache.put(event.request, responseToCache));
             }
-        }
 
-        return await caches.match(event.request);
-    }());
+            return response;
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
+      return await caches.match(event.request);
+    })()
+  );
 });

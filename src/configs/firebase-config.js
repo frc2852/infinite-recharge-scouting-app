b1ecd7a17f10d4bd513c8442112bff4e94c2b1f2
lucs100 <lucs100d@gmail.com>
2020-02-10 17:02:09 -0500

    Updated broken robot emoji

---
+++ src/configs/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAeG697qsKGtAYKaozDNflZYAIElYfou-w',
+  authDomain: 'frc-2020-scouting.firebaseapp.com',
+  databaseURL: 'https://frc-2020-scouting.firebaseio.com',
+  projectId: 'frc-2020-scouting',
+  storageBucket: 'frc-2020-scouting.appspot.com',
+  messagingSenderId: '63114247978',
+  appId: '1:63114247978:web:c257b4f61d522ad30ca853',
+};
